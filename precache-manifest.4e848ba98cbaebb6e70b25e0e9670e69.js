3e53d73f4f6c88b889d5b3c42dd418b38533b90a
Ojani Figueroa <ojanifigueroa@Ojanis-MBP.lan>
2020-08-21 19:48:06 -0400

    Updates

---
+++ precache-manifest.4e848ba98cbaebb6e70b25e0e9670e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e71e0532c918238cae43d23c6f3c2a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8768b30e5af28e31a880",
+    "url": "./static/css/main.5d1ae688.chunk.css"
+  },
+  {
+    "revision": "c5f6a20f5f7f534392fe",
+    "url": "./static/js/2.728dec45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.728dec45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8768b30e5af28e31a880",
+    "url": "./static/js/main.a1a93ef5.chunk.js"
+  },
+  {
+    "revision": "a4bf63015f1542544271",
+    "url": "./static/js/runtime-main.58276d41.js"
+  }
+]);
